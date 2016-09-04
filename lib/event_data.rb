1c233f2b20373193223d4268934fec90c27f0bc6
Morgan Slimak <morganslimak@gmail.com>
2016-09-04 16:53:48 -0600

    find method works for first names

--- lib/event_data.rb
+++ lib/event_data.rb
@@ -1,11 +1,18 @@
 require "csv"
 
 class EventData
-  attr_reader :contents
+  attr_reader :contents, :results
 
   def load(file)
     @contents = CSV.open file,
       headers: true, header_converters: :symbol
+
+  end
+
+  def find(name)
+    @results = @contents.select do |row|
+      row[:first_name].upcase == name.upcase
+    end
   end
 
 end
--- test/event_data_test.rb
+++ test/event_data_test.rb
@@ -3,9 +3,18 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class EventDataTest < Minitest::Test
+
   def test_load_small_csv
     data = EventData.new()
     data.load("event_attendees.csv")
     assert_equal CSV, data.contents.class
   end
+
+  def test_find_data_based_on_first_name
+    data = EventData.new()
+    data.load("full_event_attendees.csv")
+    data.find('John')
+    assert_equal 63, data.results.count
+  end
+
 end
