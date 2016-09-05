7984dc471977ce6bfeacc3d0fec04d6dc03c844d
Morgan Slimak <morganslimak@gmail.com>
2016-09-04 18:46:08 -0600

    find works for most attributes except zipcodes

--- lib/event_data.rb
+++ lib/event_data.rb
@@ -1,18 +1,21 @@
 require "csv"
 
 class EventData
-  attr_reader :contents, :results
+  attr_reader :results, :contents
 
   def load(file)
-    @contents = CSV.open file,
+    @contents = CSV.read file,
       headers: true, header_converters: :symbol
-
   end
 
-  def find(name)
+  def find(attribute, criteria)
     @results = @contents.select do |row|
-      row[:first_name].upcase == name.upcase
+      row[attribute].to_s.upcase == criteria.upcase
     end
+  end
+
+  def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
   end
 
 end
--- test/event_data_test.rb
+++ test/event_data_test.rb
@@ -7,14 +7,25 @@ class EventDataTest < Minitest::Test
   def test_load_small_csv
     data = EventData.new()
     data.load("event_attendees.csv")
-    assert_equal CSV, data.contents.class
+    assert_equal CSV::Table, data.contents.class
   end
 
   def test_find_data_based_on_first_name
     data = EventData.new()
     data.load("full_event_attendees.csv")
-    data.find('John')
+    data.find(:first_name, 'John')
     assert_equal 63, data.results.count
+    data.find(:first_name, 'Mary')
+    assert_equal 16, data.results.count
+  end
+
+  def test_find_data_works_for_all_attributes
+    data = EventData.new()
+    data.load("full_event_attendees.csv")
+    data.find(:city, 'Salt Lake City')
+    assert_equal 13, data.results.count
+    data.find(:zipcode, '07306')
+    assert_equal 13, data.results.count
   end
 
 end
