30013fc856f37f19b0dfe08c4d0fb9b09c5ea4bf
Morgan Slimak <morganslimak@gmail.com>
2016-09-08 20:49:05 -0600

    refactored the printer methods

--- lib/event_queue.rb
+++ lib/event_queue.rb
@@ -9,6 +9,8 @@ class EventQueue
     @results = []
     @attributes = ["last_name", "first_name", "email", "zipcode", "city",
                   "state", "address", "phone", "district"]
+    @headers = ["LAST NAME", "FIRST NAME", "EMAIL", "ZIPCODE", "CITY",
+               "STATE", "ADDRESS", "PHONE", "DISTRICT"]
   end
 
   def clear
@@ -20,46 +22,58 @@ class EventQueue
   end
 
   def print
-    headers = ["LAST NAME", "FIRST NAME", "EMAIL", "ZIPCODE", "CITY",
-              "STATE", "ADDRESS", "PHONE", "DISTRICT"]
     max_lengths = find_max_lengths
     return if max_lengths.empty?
-    headers_and_lengths = headers.zip(max_lengths)
+    print_headers(max_lengths)
+    print_attributes(max_lengths)
+  end
+
+  def print_headers(max_lengths)
+    headers_and_lengths = @headers.zip(max_lengths)
     formatted_headers = ""
     headers_and_lengths.each do |header, length|
       formatted_headers += header.ljust(length)
     end
     puts formatted_headers
+  end
+
+  def print_attributes(max_lengths)
     attributes_and_lengths = @attributes.zip(max_lengths)
     @results.each do |attendee|
-      formatted_attributes = ""
-      attributes_and_lengths.each do |attribute, length|
-        formatted_attributes += attendee.send(attribute).ljust(length)
-      end
-      puts formatted_attributes
+      print_attendee(attributes_and_lengths, attendee)
     end
+  end
+
+  def print_attendee(attributes_and_lengths, attendee)
+    formatted_attributes = ""
+    attributes_and_lengths.each do |attribute, length|
+      formatted_attributes += attendee.send(attribute).ljust(length)
+    end
+    puts formatted_attributes
   end
 
   def find_max_lengths
     max_lengths = []
     @attributes.each do |attribute|
       break if @results.empty?
-      sorted_by_attribute = @results.sort_by do |attendee|
-        attendee.send(attribute).length
-      end
-      if sorted_by_attribute.last.send(attribute).length + 5 > 11
-        max_lengths << sorted_by_attribute.last.send(attribute).length + 5
-      else
-        max_lengths << 11
-      end
+      attributes_by_length = sort_attributes_by_length(attribute)
+      max_length = attributes_by_length.last.send(attribute).length + 5
+      max_length > 11 ? max_lengths << max_length : max_lengths << 11
     end
     return max_lengths
+  end
+
+  def sort_attributes_by_length(attribute)
+    @results.sort_by do |attendee|
+      attendee.send(attribute).length
+    end
   end
 
   def district
     if @results.length < 10
       @results.each do |attendee|
-        url = "https://congress.api.sunlightfoundation.com/districts/locate?zip=#{attendee.zipcode}&apikey=d2feb7fe2971453889f66b544ee396c4"
+        url = "https://congress.api.sunlightfoundation.com/districts/locate?"\
+          "zip=#{attendee.zipcode}&apikey=d2feb7fe2971453889f66b544ee396c4"
         response = Net::HTTP.get_response(URI.parse(url))
         sunlight_data = JSON.parse(response.body)
         unless sunlight_data["results"].empty?
